d1d8f4ce2682e06a371d21d3a0ba5cd74f7a691f
xNoelSV <noelsava25@gmail.com>
2024-01-31 23:54:37 +0100

    RED SOCIAL: Reinstall mongoose-pagination

--- API_Rest_RED-SOCIAL/database/connection.js
+++ API_Rest_RED-SOCIAL/database/connection.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connection = async() => {
     try {
+        // await mongoose.connect("mongodb+srv://noelsava25:sa8ApXluKgyaZDgN@blog.otgkqmv.mongodb.net/");
         await mongoose.connect("mongodb://localhost:27017/mi_redsocial");
         console.log("Conectado correctamente a bd: mi_redsocial");
     } catch (error) {
@@ -9,5 +10,6 @@ const connection = async() => {
         throw new Error("No se ha podido conectar a la base de datos!!")
     }
 }
+// sa8ApXluKgyaZDgN
 
 module.exports = connection;
