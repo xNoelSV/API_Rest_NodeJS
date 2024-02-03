eb74c5c1df9ef9fe7c435ea58dfb03e07f30d117
xNoelSV <noelsava25@gmail.com>
2024-02-03 10:43:32 +0100

    RED SOCIAL: Conexión BD

--- API_Rest_RED-SOCIAL/database/connection.js
+++ API_Rest_RED-SOCIAL/database/connection.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connection = async() => {
     try {
-        // await mongoose.connect("mongodb+srv://noelsava25:sa8ApXluKgyaZDgN@blog.otgkqmv.mongodb.net/");
-        await mongoose.connect("mongodb://localhost:27017/mi_redsocial");
+        await mongoose.connect("mongodb+srv://noelsava25:sa8ApXluKgyaZDgN@blog.otgkqmv.mongodb.net/mi_redsocial");
+        //await mongoose.connect("mongodb://localhost:27017/mi_redsocial");
         console.log("Conectado correctamente a bd: mi_redsocial");
     } catch (error) {
         console.log(error.toString());
